5e79d70b4fcce33215a56b0b96bdbbbeb63e1115
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-01-15 02:37:48 +0530

    Linting file

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true, // enable ES6 features
+    },
+    parserOptions: {
+        ecmaVersion: 2020, // Use the latest ECMAScript version
+        sourceType: 'module', // Set the source type to module
+        ecmaFeatures: {
+            jsx: true, // Allow JSX
+        },
+    },
+};
